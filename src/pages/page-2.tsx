ba055939cb7e162815d723786610be890968d9b9
Wiliam De Schepper <info@4rweb.net>
2020-05-15 02:43:43 -0300

    Segundo Mugido

--- src/pages/page-2.tsx
+++ src/pages/page-2.tsx
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const SecondPage = (props: PageProps) => (
   <Layout>
     <SEO title="Mugu" />
-    <p style={{fontSize: 30}}>MuguGenerator é um gerador de mugidos que converte o texto em mugido, o idioma oficial do bolsoloides que vivem defendendo as merdas que o governo faz, principalmente o lider Jair BolsoAsno! Como essa gente não adianta argumentar, então talvez enviar algo no seu idioma original!</p>
+    <p style={{fontSize: 30}}>MuMuGenerator é um gerador de mugidos que converte o texto em mugido, o idioma oficial do bolsoloides que vivem defendendo as merdas que o governo faz, principalmente o lider Jair BolsoAsno! Como essa gente não adianta argumentar, então talvez enviar algo no seu idioma original!</p>
     <Link to="/">Voltar a mugir</Link>
   </Layout>
 )
